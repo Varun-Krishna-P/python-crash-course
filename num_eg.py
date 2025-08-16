7119c30d86722df0716345213d2a90f70a2cd340
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2025-08-16 08:17:25 +0000

    numbers example.

---
+++ num_eg.py
@@ -0,0 +1,10 @@
+print(2 + 3)
+print (10 - 5)
+print(2 / 10)
+print(2 * 10)
+print(2 + 3 * 4)
+print( 2 ** 2)
+print((2 + 3) * 4)
+universe_age = 14_000_000_000
+print(universe_age)
+x, y, z = 0, 0, 0
